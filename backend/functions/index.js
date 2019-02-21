015cd41e855c84ac89ed77175e7ea4b1af88542a
DESKTOP-4PMUB76\최종원 <cjw0515@naver.com>
2019-02-21 17:03:27 +0900

    express 모듈 추가

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,4 +1,17 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');    //firebase 어드민 sdk. 실시간 데이터베이스 처리 및 인증 수행
+admin.initializeApp(functions.config().firebase);   //admin인스턴스 초기화
+
+const express = require('express');
+const app = express();
+const cors = require('cors')({origin: true});
+app.use(cors);
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+exports.v1 = functions.https.onRequest(app);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
