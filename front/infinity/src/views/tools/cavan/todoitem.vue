fda7b50da4dfae88d52f1ba0e0e54916b6d58202
daniel <daniel@DESKTOP-6N9LEI7>
2019-03-17 02:05:23 +0900

    SCRUM BOARD

---
+++ front/infinity/src/views/tools/cavan/todoitem.vue
@@ -0,0 +1,39 @@
+<template>
+    
+        <li class="list-group-item" >            
+            {{tododata.titledata}}
+            {{tododata.todoval}}
+
+
+            <span class="badge">{{tododata.pri}}</span>
+            <button class="float-right"  @click="delbtn(tododata.uid)" >삭제</button>
+          </li>
+
+</template>
+
+<script>
+import eventBus from '../../../EventBus.js';
+
+export default {
+    props:["tododata"],
+    data() {
+        return {
+            
+        }
+    },
+    mounted: {
+      
+    },
+    methods: {
+        delbtn(uid){
+            if(confirm("삭제하시겠습니까?")){
+                eventBus.$emit("DeleteCavan",uid);                 
+            }
+        }
+    },  
+}
+</script>
+
+<style>
+
+</style>
