8a0e3dcaa689cf20e91ce62e45b59c3dce967553
jw <cjw0515@naver.com>
2019-03-19 23:03:09 +0900

    api디자인

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,45 +1,13 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');    //firebase 어드민 sdk. 실시간 데이터베이스 처리 및 인증 수행
-admin.initializeApp(functions.config().firebase);   //admin인스턴스 초기화
-
+const { functions } = require('./config/config.js')
 const express = require('express');
 const app = express();
 const cors = require('cors')({origin: true});
+const todolist = require('./routes/todolist/')
+
 app.use(cors);
+app.use('/todolist', todolist)
 
-app.get('/', (req, res) => {
-    res.send('hello world111');
-})
-
-app.get('/todolist', (req, res) => {
-    let todoRef = admin.database().ref('todolist');
-    todoRef.once('value', (snapshot) => {
-        let items = new Array();
-        snapshot.forEach((childSnapshot) => {
-            let todo = childSnapshot;
-            items.push(todo);
-        })
-        res.header('Content-Type', 'application/json; charset=utf-8')
-        res.send({todoList: items});
-    })
-});
-
-app.post('/todolist', (req, res) => {    
-    // res.send(req.body)
-    // let cname = req.params.cname;
-    let message = {
-        date: new Date().toJSON(),
-        todo: '밥하기',
-        user: '222'
-        // body: req.body.body,
-        // user: req.user        
-    };
-    let messagesRef = admin.database().ref(`todolist`);
-    messagesRef.push(message);
-    res.header('Content-Type', 'application/json; charset=utf-8');
-    res.status(201).send({result: "ok"});
-});
-
+// vvvvvvvvvvvvvvvvvvvv  이하 정리부탁드립니다.  vvvvvvvvvvvvvvvvvvvvvvvv
 /* kkitodo 
 C R U D
 
@@ -222,9 +190,9 @@ app.post('/cavan',(req,res)=>{
 
 exports.v1 = functions.https.onRequest(app);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
